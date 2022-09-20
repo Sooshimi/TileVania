17860194cce6966ed0649e9417ebdd8116af3c1f
Sooshimi <masonchee93@gmail.com>
2022-09-20 21:04:51 +0100

    Added climbing and cinemachine follow cam

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,31 +5,39 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 8f;
     [SerializeField] float jumpPower = 25f;
+    [SerializeField] float climbSpeed = 5f;
 
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
+    CapsuleCollider2D myCapsuleCollider2D;
+    float gravityScaleAtStart;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCapsuleCollider2D = GetComponent<CapsuleCollider2D>();
+        gravityScaleAtStart = myRigidbody.gravityScale;
     }
 
     void Update()
     {
         Run();
         FlipSprite();
+        ClimbLadder();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
     }
 
     void OnJump(InputValue value)
     {
+        // first get layer mask of "Ground", then use that layer mask as parameter for IsTouchingLayers() function
+        if (!myCapsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) {return ;}
+
         if (value.isPressed)
         {
             myRigidbody.velocity += new Vector2 (0f, jumpPower);
@@ -54,5 +62,22 @@ public class PlayerMovement : MonoBehaviour
 
         if (playerHasHorizontalSpeed) 
             transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+    }
+
+    void ClimbLadder()
+    {
+        if (!myCapsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myRigidbody.gravityScale = gravityScaleAtStart; // normal gravity when not climbing ladder
+            myAnimator.SetBool("isClimbing", false);
+            return;
+        }
+
+        Vector2 climbVelocity = new Vector2 (myRigidbody.velocity.x, moveInput.y * climbSpeed);
+        myRigidbody.velocity = climbVelocity;
+        myRigidbody.gravityScale = 0f; // no gravity when climbing ladder
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
     }
 }

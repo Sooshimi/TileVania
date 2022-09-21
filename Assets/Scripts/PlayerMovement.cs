fc0f0d537eee3bdbef2bf7ca0e9209627fc7cb0c
Sooshimi <masonchee93@gmail.com>
2022-09-21 18:26:47 +0100

    Added state driven cameras and bouncing mechanics

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,14 +10,16 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
-    CapsuleCollider2D myCapsuleCollider2D;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeetCollider;
     float gravityScaleAtStart;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        myCapsuleCollider2D = GetComponent<CapsuleCollider2D>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
         gravityScaleAtStart = myRigidbody.gravityScale;
     }
 
@@ -36,7 +38,7 @@ public class PlayerMovement : MonoBehaviour
     void OnJump(InputValue value)
     {
         // first get layer mask of "Ground", then use that layer mask as parameter for IsTouchingLayers() function
-        if (!myCapsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) {return ;}
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {return ;}
 
         if (value.isPressed)
         {
@@ -66,7 +68,7 @@ public class PlayerMovement : MonoBehaviour
 
     void ClimbLadder()
     {
-        if (!myCapsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
         {
             myRigidbody.gravityScale = gravityScaleAtStart; // normal gravity when not climbing ladder
             myAnimator.SetBool("isClimbing", false);
